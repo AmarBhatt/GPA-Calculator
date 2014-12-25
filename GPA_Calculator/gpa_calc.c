b0d0a5f56b31e5fae601d44787b7332220f762dc
Amar Bhatt <bhatt.amar.a@gmail.com>
2014-12-24 19:04:18 -0500

    Added data validation

--- GPA_Calculator/gpa_calc.c
+++ GPA_Calculator/gpa_calc.c
@@ -2,18 +2,18 @@
  * gpa_calc.c
  *
  *  Created on: Dec 22, 2014
- *      Author: Amar
+ *      Author: Amar Bhatt
  */
 
 
 #include <stdio.h>
 #include <string.h>
 enum {
-	A,Am,Bp,B,Bm,Cp,C,Cm,D,F,W
+	A,Am,Bp,B,Bm,Cp,C,Cm,D,F
 } grade_t;
 
 struct course {
-	char name[20];
+	char name[50];
 	int credits;
 	float grade;
 };
@@ -56,14 +56,12 @@ float getGradeValue(int grade){
 			return 1.000;
 		case F :
 			return 0.000;
-		case W :
-			return 0.000;
 	}
 	return -1;
 }
 
 void displayGradeMenu() {
-	puts("(0) A\n(1) A-\n(2) B+\n(3) B\n(4) B-\n(5) C+\n(6) C\n(7) C-\n(8) D\n(9) F\n(10) W\n");
+	puts("(0) A\n(1) A-\n(2) B+\n(3) B\n(4) B-\n(5) C+\n(6) C\n(7) C-\n(8) D\n(9) F/W\n");
 	return;
 }
 void getCourses(struct course courses[], int *size){
@@ -72,32 +70,45 @@ void getCourses(struct course courses[], int *size){
 	int credit;
 	char stop = 'y';
 	int i = 0;
+	int result;
+	char temp[50];
 
 	puts("Get Courses!\n");
 	while((stop == 'y') && (i < 8)){
 		puts("Enter course name:");
-		fgets(c.name, 20, stdin);
+		fgets(c.name, 50, stdin);
 		puts("Enter number of credits:");
-		scanf("%d", &credit);
+		fgets(temp, 50, stdin);
+		result = sscanf(temp, "%2d", &credit);
+		while (result != 1){
+			puts("Read failed. Please enter a valid number:\n");
+			fgets(temp, 50, stdin);
+			result = sscanf(temp, "%2d", &credit);
+		}
 		c.credits = credit;
 		puts("Enter expected grade:");
 		displayGradeMenu();
-		scanf("%d", &grade);
+		fgets(temp, 50, stdin);
+		result = sscanf(temp,"%1d", &grade);
+		while (result != 1){
+			puts("Read failed. Please enter a valid number:\n");
+			fgets(temp, 50, stdin);
+			result = sscanf(temp,"%1d", &grade);
+		}
 		c.grade = getGradeValue(grade);
-		//validate c
 		strcpy(courses[i].name, c.name);
 		courses[i].credits = c.credits;
 		courses[i].grade = c.grade;
 		i++;
-		puts("Course Added!\nTo add another course press (y).\n Press Enter to exit.\n");
-		getchar();
+		puts("Course Added!\nTo add another course press (y).\nPress Enter to exit.\n");
 		stop = getchar();
 		getchar();
 	}
 	*size = i;
 }
 
-
+// Remember to validate data
+// Add a way for users to enter a GPA threshold and calculate scenarios to be above the threshold
 int main (int argc, char ** argv){
 	float grade_points;
 	int result;
@@ -107,58 +118,49 @@ int main (int argc, char ** argv){
 	struct course courses[8];
 	float gpa;
 	char c;
+	char temp[50];
 
 	puts("Welcome to GPA Calculator!\n");
 	puts("Enter your total grade points:\n");
+	fgets(temp, 50, stdin);
+	result = sscanf(temp,"%f",&grade_points);
 
-	result = scanf("%f",&grade_points);
-	if (result == EOF){
-		puts("Read failed");
-	}else if (result == 0){
-		puts("Nothing was read");
-	}else{
-		printf("You entered: %f\n", grade_points);
-		getchar(); // Clear buffer of the \n
-
+	while (result != 1){
+		puts("Read failed. Please enter a valid number:\n");
+		fgets(temp, 50, stdin);
+		result = sscanf(temp,"%f",&grade_points);
 	}
+
+	printf("You entered: %.3f grade points\n", grade_points);
+
 	puts("Enter your total credits taken:\n");
 
-	result = scanf("%d",&units);
-	if (result == EOF){
-		puts("Read failed");
-	}else if (result == 0){
-		puts("Nothing was read");
-	}else{
-		printf("You entered: %d\n", units);
-		getchar(); // Clear buffer of the \n
+	fgets(temp, 50, stdin);
+	result = sscanf(temp,"%d",&units);
+
+	while (result != 1){
+		puts("Read failed. Please enter a valid number:\n");
+		fgets(temp, 50, stdin);
+		result = sscanf(temp,"%d",&units);
 	}
 
-	printf("Your Current GPA is: %f\n", grade_points/units);
+	printf("You entered: %d credits\n", units);
 
-	/*puts("Enter grade:");
-	int grade;
-	float real;
-	displayGradeMenu();
-	scanf("%d", &grade);
 
-	real = getGradeValue(grade);
-	printf("Grade is %f", real);
-	getchar();*/
-	puts("To enter to courses press (y).\n");
+	printf("Your Current GPA is: %.3f\n", grade_points/units);
+
+	puts("To enter courses to calculate your estimated GPA press (y).\nOtherwise press enter.\n");
 	c = getchar();
 	if( c == 'y') {
 		getchar();
 		getCourses(courses, &numCourses);
+		puts("List of Courses:\n");
+		for (i = 0; i < numCourses; i++){
+			printf("%d). %s Credits: %d Expected Grade: %.3f\n",i+1,courses[i].name, courses[i].credits, courses[i].grade);
+		}
+		gpa = calculateGPA(courses, numCourses, grade_points, units);
+		printf("Your estimated GPA is %.3f\n", gpa);
 	}
-	for (i = 0; i < numCourses; i++){
-		printf("Course %d: %s Credits %d Expected Grade %f\n",i,courses[i].name, courses[i].credits, courses[i].grade);
-	}
-
-	gpa = calculateGPA(courses, numCourses, grade_points, units);
-	// Remember to validate data
-	// Add a way for users to enter a GPA threshold and calculate scenarios to be above the threshold
-
-	printf("Your estimated GPA is %f\n", gpa);
 	puts("Press Enter to Exit...");
 	getchar();
 
